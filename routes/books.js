af83354a170cfcf198535beba776378573fe639b
Sivateja <57987686+sivatejaa@users.noreply.github.com>
2023-04-05 19:49:57 -0500

    added books.js

---
+++ routes/books.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/', function (req, res) {
+    res.render('books', { title: 'Search Results - Books' });
+  });
+
+  module.exports = router;
